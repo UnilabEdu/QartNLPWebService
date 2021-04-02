7481ab85ef1064237ad236f024803c388698f874
beqaratiani <beqa.ratiani5@gmail.com>
2021-04-02 20:45:41 +0400

    done analyze button

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,14 +1,18 @@
-const btn = document.getElementById('analyze-button');
-const grammarSection = document.getElementById('analyze-section');
-const clearBtn = document.getElementsByClassName('clear-btn');
-btn.addEventListener('click', ()=>{
-    grammarSection.classList.toggle("display-flex");
-    grammarSection.classList.toggle("display-flex");
-    if(btn.innerHTML == 'გაანალიზე'){
-        btn.innerHTML = 'გაასუფთავე';
-        btn.style.background = 'gray';
-    }else{
-        btn.innerHTML = 'გაანალიზე';
-        btn.style.background = '#172224';
-    }
-});
+function analyzeButton(){
+    const btn = document.getElementById('analyze-button');
+    const grammarSection = document.getElementById('analyze-section');
+    const clearBtn = document.getElementById('clear-icon');
+    btn.addEventListener('click', ()=>{
+        grammarSection.classList.toggle("display-flex");
+        clearBtn.classList.toggle("display-flex");
+        if(btn.innerHTML == 'გაანალიზე'){
+            btn.innerHTML = 'გაასუფთავე';
+            btn.style.background = '#707070';
+        }else{
+            btn.innerHTML = 'გაანალიზე';
+            btn.style.background = '#172224';
+        }
+    });
+}
+
+analyzeButton();
