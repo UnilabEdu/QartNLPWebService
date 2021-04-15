30ea2b7032d6d7f9fa41fe9df1cbb97545408eed
beqaratiani <beqa.ratiani5@gmail.com>
2021-04-15 16:35:01 +0400

    4th page js fix

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,21 +1,23 @@
-function analyzeButton(){
-    const btn = document.getElementById('analyze-button');
-    const grammarSection = document.getElementById('analyze-section');
-    const clearBtn = document.getElementById('clear-icon');
-    btn.addEventListener('click', ()=>{
-        grammarSection.classList.toggle("display-flex");
-        clearBtn.classList.toggle("display-flex");
-        if(btn.innerHTML == 'გაანალიზე'){
-            btn.innerHTML = 'გაასუფთავე';
-            btn.style.background = '#707070';
-        }else{
-            btn.innerHTML = 'გაანალიზე';
-            btn.style.background = '#172224';
-        }
-    });
+var sPath = window.location.pathname;
+var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+if(sPage == "about.html"){
+    function analyzeButton(){
+        const btn = document.getElementById('analyze-button');
+        const grammarSection = document.getElementById('analyze-section');
+        const clearBtn = document.getElementById('clear-icon');
+        btn.addEventListener('click', ()=>{
+            grammarSection.classList.toggle("display-flex");
+            clearBtn.classList.toggle("display-flex");
+            if(btn.innerHTML == 'გაანალიზე'){
+                btn.innerHTML = 'გაასუფთავე';
+                btn.style.background = '#707070';
+            }else{
+                btn.innerHTML = 'გაანალიზე';
+                btn.style.background = '#172224';
+            }
+        });
+    }
 }
-
-analyzeButton();
 
 const loginBtn = document.querySelector('.login');
 const loginContent = document.querySelector('.login-bg');
