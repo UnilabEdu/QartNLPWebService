38bca45db76aa0b637e37bb4d5c70b5c279a98da
beqaratiani <beqa.ratiani5@gmail.com>
2021-04-21 18:48:52 +0400

    empty textarea error catch

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -2,15 +2,22 @@ const btn = document.getElementById('analyze-button');
 const grammarSection = document.getElementById('analyze-section');
 const clearIcon = document.getElementById('clear-icon');
 const clearText = document.getElementById('analyze-text');
+const grammarText = document.getElementById('prtext');
 if (btn){
     btn.addEventListener('click', ()=>{
-        grammarSection.classList.toggle("display-flex");
-        clearIcon.classList.toggle("display-flex");
         if(clearText.innerText == 'გაანალიზე'){
-            clearText.innerText = 'გაასუფთავე';
-            btn.style.background = '#707070';
-            btn.style.padding = "8px";
+            if (prtext.value == '') {
+                alert('გთხოვთ, შეიყვანეთ ტექსტი.');
+            }else{
+                grammarSection.classList.toggle("display-flex");
+                clearText.innerText = 'გაასუფთავე';
+                clearIcon.classList.toggle("display-flex");
+                btn.style.background = '#707070';
+                btn.style.padding = "8px";
+            }
         }else{
+            clearIcon.classList.toggle("display-flex");
+            grammarSection.classList.toggle("display-flex");
             clearText.innerText = 'გაანალიზე';
             btn.style.background = '#172224';
             btn.style.padding = "8px 46px";
