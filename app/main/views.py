04e6ff5babe873b72282b9473c4db6793d545be1
David <79374141+davidunilab@users.noreply.github.com>
2021-05-19 23:26:24 +0400

    Added jinjas (#53)
    * log out
    
    * hover+fixed font
    
    * converted templates to ninja
    
    * changed assets folder root
    
    ასეთიები ჩავაგდე სტატიკებში რადგანაც სხვანაირად ვერ დავანახე
    
    Co-authored-by: beqaratiani <61917204+beqaratiani@users.noreply.github.com>
    Co-authored-by: Your Name <coller.vato@gmail.com>
    Co-authored-by: Cube00 <74175522+Cube00@users.noreply.github.com>
    Co-authored-by: Kristina <33975427+kristina-makalatia@users.noreply.github.com>
    Co-authored-by: David <Davidchincharashvili@gmail.com>

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+from app.main.temp_data import people, block_files # TODO: მონაცემების წამოღება ბაზიდან
 main_blueprint = Blueprint('main',
                            __name__,
                            template_folder='templates'
@@ -7,16 +7,19 @@ main_blueprint = Blueprint('main',
 
 
 @main_blueprint.route('/', methods=['GET', 'POST'])
+@main_blueprint.route('/home', methods=['GET', 'POST'])
 def home():
-    pass
+    return render_template('home.html')
 
 
 @main_blueprint.route('/documentation', methods=['GET', 'POST'])
 def documentation():
-    pass
+    return render_template('files.html', block_files=block_files)
 
 
 @main_blueprint.route('/about_us', methods=['GET', 'POST'])
 def about_us():
-    pass
+    return render_template('about.html', people=people)
+
+
 
--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ class Config(object):
 
     # Flask settings
     CSRF_ENABLED = True
-
+    DEBUG = True
     # Flask-SQLAlchemy settings
     SECRET_KEY = os.environ.get('SECRET_KEY', 'appsecretkey')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', f'sqlite:///{os.path.join(basedir, "data.db")}')
