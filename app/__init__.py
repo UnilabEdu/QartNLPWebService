62893f07c5085004618129db36268c0dd551b5a9
David <Davidchincharashvili@gmail.com>
2021-05-19 09:36:24 +0400

    converted templates to ninja

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
-
-app = Flask(__name__)
-
+from app.temp_data import people, block_files
+app = Flask(__name__, static_url_path='/static')
+# app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+@app.route('/home')
+def home():
     return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', people=people)
+
+
+@app.route('/files')
+def files():
+    return render_template('files.html', block_files=block_files)
 
