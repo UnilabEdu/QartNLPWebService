06c940db32e4052fb356e09c6c92141fb7396e45
David <Davidchincharashvili@gmail.com>
2021-07-04 06:53:16 +0400

    moved tagging to main blueprint - moved forms - in html replaced bootstrap with skeleton css

--- app/static/JS/tagging-script.js
+++ app/static/JS/tagging-script.js
@@ -1,13 +1,22 @@
-console.log("looks like it's working")
-var resetButton = document.getElementById('reset_btn');
-var words = document.querySelectorAll('span');
+// SETTINGS
+const debug = 1;
 
-var words_list = document.getElementById('words_list');
+// VARS
+const resetButton = document.getElementById('reset_btn');
+const words = document.querySelectorAll('span');
+const words_list = document.getElementById('words_list');
+
+// FN-s
+function clog(){
+    if (debug){
+        console.log(...arguments)
+    }
+}
 
 function updateWordsList(){
-    text_data = " " + this.textContent
+    let text_data = " " + this.textContent
 
-    console.log("word has been pressed")
+    clog("word has been pressed")
     if (words_list.textContent === 'empty'){
         words_list.textContent = text_data;
     }
@@ -20,8 +29,10 @@ function resetButtonOnClick(){
     words_list.textContent = "empty";
 }
 
+
+// EVENT LISTENERS
 resetButton.addEventListener('click', resetButtonOnClick);
 
-for(var i = 0; i < words.length; i++){
+for(let i = 0; i < words.length; i++){
     words[i].addEventListener('click', updateWordsList );
 }
