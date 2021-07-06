bc4b19892560ff58f9ecd9321bd13c976f7e4a0e
David <79374141+davidunilab@users.noreply.github.com>
2021-07-06 12:16:44 +0400

    Updated tagging.js file (#87)
    * create details page
    
    * added files to jinja folder 
    
    changed one file for babel
    
    * bebelified jinja files
    
    * Updated registration modal
    
    * changed icons position
    
    * fix merge
    
    * removed old password field
    
    * validation and favicon
    
    * upload button
    
    * added html structure + bootstrap for tagging
    
    * added social link to about page
    
    * edited login forms
    
    * added missed changes from front
    
    * moved tagging to main blueprint
    - moved forms
    - in html replaced bootstrap with skeleton css
    
    * updated tagging js
    
    Co-authored-by: David <Davidchincharashvili@gmail.com>
    Co-authored-by: Your Name <coller.vato@gmail.com>
    Co-authored-by: Cube00 <74175522+Cube00@users.noreply.github.com>
    Co-authored-by: NokaK <33997372+NokaK@users.noreply.github.com>
    Co-authored-by: beqaratiani <beqa.ratiani5@gmail.com>
    Co-authored-by: beqaratiani <61917204+beqaratiani@users.noreply.github.com>
    Co-authored-by: Kristina <33975427+kristina-makalatia@users.noreply.github.com>

--- app/static/JS/tagging-script.js
+++ app/static/JS/tagging-script.js
@@ -1,61 +1,51 @@
-// SETTINGS
-const debug = 1;
-
 // VARS
 const resetButton = document.getElementById('reset_btn');
 const words = document.querySelectorAll('span');
 const words_list = document.getElementById('words_list');
 
+let taggedWordArray = [] // array of word objects
+let taggedWords = [] // array of words (right block)
+
+
+
+
 // FN-s
-function clog(){
-    if (debug){
-        console.log(...arguments)
-    }
-}
-
-const taggedWordArray = []
-let taggedWords = []
-
 function updateWordsList() {
-    let wordId = this.id.split("w")[1]
-    let wordContent = this.textContent
-
 
     let wordObject = {
-        id: wordId,
-        content: wordContent
+        id: this.id.split("w")[1],
+        content: this.textContent
     }
 
-    if (taggedWordArray.length === 0) {
+    let is_taggedWordArray_empty = taggedWordArray.length === 0;
+
+    if (is_taggedWordArray_empty) {
+        // just add clicked word if it's the first one;
         taggedWordArray.push(wordObject)
+
     } else {
+
         let lastWordID = taggedWordArray[taggedWordArray.length - 1].id
+        let firstWordID = taggedWordArray[0].id
 
-        if (Math.abs(lastWordID - wordId) === 1) {
-            taggedWordArray.push(wordObject)
+        let is_right_neighbor = Math.abs(parseInt(lastWordID) - parseInt(wordObject.id)) === 1
+        let is_left_neighbor = Math.abs(parseInt(firstWordID) - parseInt(wordObject.id)) === 1
+
+        if (is_right_neighbor || is_left_neighbor) {
+            taggedWordArray.push(wordObject);
+            taggedWordArray.sort((a, b) => a.id - b.id );
         }
-
-        taggedWordArray.sort(function(a, b) {
-            return a.id - b.id ;
-        });
     }
+    updateWordListView(); // right block
 
-
-
-
-    console.log(taggedWordArray)
-    // clog(wordObject+" has been pressed")
-
-    getWordsFromTaggedWordArray()
-    words_list.textContent = taggedWords
     console.log(taggedWordArray)
 }
 
-function getWordsFromTaggedWordArray(){
+
+function updateWordListView(){
     taggedWords = []
-    for (const obj of taggedWordArray) {
-        taggedWords.push(obj.content)
-    }
+    for (const obj of taggedWordArray) { taggedWords.push(obj.content) }
+    words_list.textContent = taggedWords.join(" ")
 }
 
 function resetButtonOnClick(){
@@ -66,6 +56,6 @@ function resetButtonOnClick(){
 // EVENT LISTENERS
 resetButton.addEventListener('click', resetButtonOnClick);
 
-for(let i = 0; i < words.length; i++){
+for( let i = 0; i < words.length; i++ ){
     words[i].addEventListener('click', updateWordsList );
 }
