9e57e62dbbcd323a456b51abbda953f2c088874b
David <79374141+davidunilab@users.noreply.github.com>
2021-07-12 20:16:38 +0400

    added init tags to tagging page (#94)
    * create details page
    
    * added files to jinja folder 
    
    changed one file for babel
    
    * bebelified jinja files
    
    * Updated registration modal
    
    * changed icons position
    
    * fix merge
    
    * removed old password field
    
    * validation and favicon
    
    * upload button
    
    * checkbox update
    
    * added html structure + bootstrap for tagging
    
    * added social link to about page
    
    * edited login forms
    
    * added missed changes from front
    
    * moved tagging to main blueprint
    - moved forms
    - in html replaced bootstrap with skeleton css
    
    * updated tagging js
    
    * working on initTags javascript
    
    * added initTags javascipt
    
    * added tostify-js to tagging.js
    
    Co-authored-by: David <Davidchincharashvili@gmail.com>
    Co-authored-by: Your Name <coller.vato@gmail.com>
    Co-authored-by: Cube00 <74175522+Cube00@users.noreply.github.com>
    Co-authored-by: NokaK <33997372+NokaK@users.noreply.github.com>
    Co-authored-by: beqaratiani <beqa.ratiani5@gmail.com>
    Co-authored-by: beqaratiani <61917204+beqaratiani@users.noreply.github.com>
    Co-authored-by: Kristina <33975427+kristina-makalatia@users.noreply.github.com>

--- app/static/JS/tagging-script.js
+++ app/static/JS/tagging-script.js
@@ -3,20 +3,19 @@ const resetButton = document.getElementById('reset_btn');
 const saveButton = document.getElementById('submit');
 const words = document.querySelectorAll('span');
 const words_list = document.getElementById('words_list');
-
+const initJson = document.getElementById('init-json');
+console.log('init_json id selected', initJson)
 let taggedWordArray = [] // array of word objects
 let taggedWords = [] // array of words (right block)
 
 
-
-
 // FN-s
-function updateWordsList() {
+function updateWordsList(wordObject) {
 
-    let wordObject = {
-        id: this.id.split("w")[1],
-        content: this.textContent
-    }
+    // let wordObject = {
+    //     id: this.id.split("w")[1],
+    //     content: this.textContent
+    // }
 
     let is_taggedWordArray_empty = taggedWordArray.length === 0;
 
@@ -34,29 +33,44 @@ function updateWordsList() {
 
         let is_in_array
 
-        for (let i of taggedWordArray){
-            if (i.id === wordObject.id){
+        for (let i of taggedWordArray) {
+            if (i.id === wordObject.id) {
                 is_in_array = true
             }
         }
 
         if (!is_in_array && (is_right_neighbor || is_left_neighbor)) {
             taggedWordArray.push(wordObject);
-            taggedWordArray.sort((a, b) => a.id - b.id );
+            taggedWordArray.sort((a, b) => a.id - b.id);
+        } else {
+            Toastify({
+              text: "შემდეგი სიტყვა უნდა იყოს ან მარჯვენა ან მარხენა მეზობელი",
+              duration: 10000,
+              // destination: "https://github.com/apvarun/toastify-js",  // link to plugin github page
+              // newWindow: true,
+              close: true,
+              gravity: "top", // `top` or `bottom`
+              position: "left", // `left`, `center` or `right`
+              backgroundColor: "linear-gradient(to right, darkred, red)",
+              stopOnFocus: true, // Prevents dismissing of toast on hover
+              onClick: function(){} // Callback after click
+            }).showToast();
+
         }
     }
     updateWordListView(); // right block
 
 }
 
-
-function updateWordListView(){
+function updateWordListView() {
     taggedWords = []
-    for (const obj of taggedWordArray) { taggedWords.push(obj.content) }
+    for (const obj of taggedWordArray) {
+        taggedWords.push(obj.content)
+    }
     words_list.textContent = taggedWords.join(" ")
 }
 
-function resetButtonOnClick(){
+function resetButtonOnClick() {
     words_list.textContent = "empty";
     taggedWordArray = []
     taggedWords = []
@@ -75,23 +89,56 @@ function saveButtonOnClick() {
     fetch('http://127.0.0.1:5000/api/', {
         method: 'POST',
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(finalJSON)
-      }).then(response => response.json())
+    }).then(response => response.json())
         .then(data => {
-          console.log('Success:', data);
+            console.log('Success:', data);
         })
         .catch((error) => {
-          console.error('Error:', error);
+            console.error('Error:', error);
         });
 }
+
+function wrap_id(ids, tag){
+    if (Array.isArray(ids) && ids.length > 1){
+        let word = document.querySelector('#w'+ids[0])
+        let wrapper = document.createElement('span')
+        wrapper.classList.add('ner-tag', 'ner-'+tag.toLowerCase())
+        word.parentNode.insertBefore(wrapper,word)
+
+
+        for (let i = ids[0]; i<=ids[1]; i++ ) {
+            wrapper.appendChild(document.querySelector('#w'+i))
+        }
+    }
+}
+
+function initTags(initJsonSelector) {
+    let tags = JSON.parse(initJsonSelector.innerHTML)
+    tags.forEach((words) => {
+        if (words.keys.length > 1) {
+            wrap_id(words.keys, words.value)
+        } else {
+            wrap_id([words.keys[0],words.keys[0]], words.value)
+        }
+    })
+}
+
 
 // EVENT LISTENERS
 resetButton.addEventListener('click', resetButtonOnClick);
 saveButton.addEventListener('click', saveButtonOnClick);
 
-for( let i = 0; i < words.length; i++ ){
-    words[i].addEventListener('click', updateWordsList );
+for (let i = 0; i < words.length; i++) {
+    words[i].addEventListener('click', function () {
+        updateWordsList({
+            id: this.id.split("w")[1],
+            content: this.textContent
+        })
+    });
 }
+
+initTags(initJson)
