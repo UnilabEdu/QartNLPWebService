d76085a06d6122e92b2341d933cf2834a9a911fa
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-07 10:54:56 +0800

    Delete app directory

--- app/commands/__init__.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from flask_script import Manager
-
-from flask_migrate import MigrateCommand
-
-manager = Manager(create_app())
-
-manager.add_command('db', MigrateCommand)
