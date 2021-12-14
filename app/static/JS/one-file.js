98fd8376b41c7230dc1db536daca96b6b43a7d90
Denissant <denissanturyan@gmail.com>
2021-12-14 18:44:10 +0400

    improved styles on files' word lemma view and escaped tags

---
+++ app/static/JS/one-file.js
@@ -0,0 +1,27 @@
+function get_word_details(wordId) {
+    let lemma = document.getElementById('word_lemma');
+    let tag = document.getElementById('word_tag');
+
+    let currentLemma = lemmaArray[wordId].lemma;
+    let currentWordTags = lemmaArray[wordId].tags;
+    if (!currentWordTags && !currentLemma) {
+        lemma.innerHTML = 'სიტყვის ლემატიზაცია ვერ შესრულდა';
+        tag.innerHTML = '';
+    } else {
+        currentWordTags = currentWordTags.split(',').join(', ');
+        currentWordTags = escapeHtml(currentWordTags);
+        lemma.innerHTML = `<b>ლემმა</b>: ${currentLemma}`;
+        tag.innerHTML = `<b>თეგები</b>: ${currentWordTags}`;
+    }
+}
+
+// utils
+function escapeHtml(string)
+{
+    return string
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+}
