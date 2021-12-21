4a6e5efd5e6c1c86b352ab789ec513ed199076d5
Denissant <denissanturyan@gmail.com>
2021-12-22 02:40:14 +0400

    deleted obsolete files

--- app/main/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class NerTagForm(FlaskForm):
-    ner_tag = SelectField("choose tag from the list", choices=[('aim', 'AIM'), ('msn', 'MSN')], validators=[DataRequired(),])
-    submit = SubmitField("save")
